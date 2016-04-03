6668ff97f65b684e48ea0a853f4f64359a5fdfb8
sebisujar <sebisujar@gmail.com>
2016-04-03 19:42:20 -0300

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function die()
+{
+  echo "${@}"
+  exit 1
+}
+
+vim_dir = "$HOME/.vim"
+
+git clone https://github.com/SebiSujar/myvim $vim_dir \
+  || die "Could not clone the repository to ${HOME}/.vim"
+
+ln -s $vim_dir $HOME/.vimrc \
+  || die "Could not clone the repository to ${HOME}/.vim"
