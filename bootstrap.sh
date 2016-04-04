e7b02359efb1724388bfbc7b2d33948d6a0464a3
Sebastian Sujarchuk <sebisujar@gmail.com>
2016-04-04 17:08:53 +0200

    install plugins in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,3 +13,6 @@ git clone --recursive https://github.com/SebiSujar/myvim $vim_dir \
 
 ln -s $vim_dir/vimrc $HOME/.vimrc \
   || die "Could not create symbolic link in ${HOME}"
+
+echo "installing plugins, this may take a while"
+echo | echo | vim +PluginInstall +qall &>/dev/null
