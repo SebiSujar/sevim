5f15defae42e89de6feaa0164c4efa4c88c1d658
SebiSujar <sebisujar@gmail.com>
2016-04-04 23:18:54 -0300

    add spinner and header to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,17 @@ function die()
   exit 1
 }
 
+echo "Welcome to..."
+cat << "EOF"
+ ______     ______     __   __   __     __    __    
+/\  ___\   /\  ___\   /\ \ / /  /\ \   /\ "-./  \   
+\ \___  \  \ \  __\   \ \ \'/   \ \ \  \ \ \-./\ \  
+ \/\_____\  \ \_____\  \ \__|    \ \_\  \ \_\ \ \_\ 
+  \/_____/   \/_____/   \/_/      \/_/   \/_/  \/_/ 
+                                                    
+EOF
+echo
+
 vim_dir="$HOME/.vim"
 
 git clone --recursive https://github.com/SebiSujar/sevim $vim_dir \
@@ -15,4 +26,19 @@ ln -s $vim_dir/vimrc $HOME/.vimrc \
   || die "Could not create symbolic link in ${HOME}"
 
 echo "installing plugins, this may take a while"
-echo | echo | vim +PluginInstall +qall &>/dev/null
+
+echo | echo | vim +PluginInstall +qall &>/dev/null &
+pid=$!
+
+#spinner
+spin='-\|/'
+
+i=0
+while kill -0 $pid 2>/dev/null
+do
+  i=$(( (i+1) %4 ))
+  printf "\r${spin:$i:1}"
+  sleep .1
+done
+
+printf "\rDone!\nTime to vim hell out of sevim!\n"
